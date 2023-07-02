59c809a2e65e68c62292d57675393027680bc7d2
aravind <dremerz.product@gmail.com>
2023-07-02 21:03:59 +0530

    m

---
+++ current/Contact Us_files/dynamic-style.php
@@ -0,0 +1,13 @@
+
+<!-- Page Bg Color -->
+  .vt-maintenance-mode {
+      }
+@media (max-width: 1199px) {
+  .no-class {}
+  .header-right .xrton-navigation,  .header-right .header-links-wrap {
+    display: none!important;
+  }
+
+}
+body, input[type="submit"], .xrton-btn span, .service-info h4, .footer-widget .widget-title{font-family: "Montserrat", Arial, sans-serif;font-style: normal;}.xrton-navigation ul, .mean-container .mean-nav ul li a{font-family: "Roboto Slab", Arial, sans-serif;font-style: normal;}.xrton-navigation .dropdown-menu, .xrton-navigation .dropdown-nav li a, .mean-container .mean-nav ul.sub-menu li a, .mean-container .mean-nav ul.dropdown-nav li a{font-family: "Roboto Slab", Arial, sans-serif;font-style: normal;}p, .xrton-topbar, .caption-subtitle, 
+.progress-counter, .news-meta ul, .quick-links, .post-date, .breadcrumb, .error-subtitle, .masonry-filters, .blog-tags a, .service-info .service-info-wrap{font-family: "Roboto Slab", Arial, sans-serif;font-style: normal;}.your-custom-class{font-family: "Roboto Slab", Arial, sans-serif;font-style: normal;}
